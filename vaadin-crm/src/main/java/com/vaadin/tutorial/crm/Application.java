f30871ba7edbc369262d449a392341fe2e52f67c
ritul3007 <ritul3007@gmail.com>
2020-05-17 09:50:52 +0530

    Added Docker and Jenkins File

--- vaadin-crm/src/main/java/com/vaadin/tutorial/crm/Application.java
+++ vaadin-crm/src/main/java/com/vaadin/tutorial/crm/Application.java
@@ -1,8 +1,18 @@
 package com.vaadin.tutorial.crm;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.vaadin.flow.server.VaadinServlet;
 
 /**
  * The entry point of the Spring Boot application.
@@ -10,8 +20,26 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean<VaadinServlet> frontendServletBean() {
+		ServletRegistrationBean<VaadinServlet> bean = new ServletRegistrationBean<>(new VaadinServlet() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void service(HttpServletRequest req, HttpServletResponse resp)
+					throws ServletException, IOException {
+				if (!serveStaticOrWebJarRequest(req, resp)) {
+					resp.sendError(404);
+				}
+			}
+		}, "/frontend/*");
+		bean.setLoadOnStartup(1);
+		return bean;
+	}
 
 }
